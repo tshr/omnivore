48726765e1cc2106702a6fa58ef4e9f7f1d275fe
tshr <tshr@Toshiros-Mac-mini.local>
2013-06-16 22:58:47 -0700

    rspec description fixes and reordering

--- spec/omnivore_spec.rb
+++ spec/omnivore_spec.rb
@@ -2,16 +2,6 @@ require 'spec_helper'
 
 describe "Omnivore" do
 
-  it "should respond to GET" do
-    get '/'
-    last_response.should be_ok
-  end
-
-  it "should return the welcome page at root" do
-    get '/'
-    last_response.body.should include "Hi. I'm Omnivore, your friendly feed cache server."
-  end
-
   context "In Cached module" do
     context "the expired? method" do
       let(:test_hash) { {} }
@@ -24,26 +14,36 @@ describe "Omnivore" do
         test_hash.expired?.should be_false
       end
 
-      it "should return false if self's updated time plus TIME_TO_LIVE is before the current time" do
+      it "should return true if self's updated time plus TIME_TO_LIVE is before the current time" do
         Timecop.freeze(Time.now) do
           test_hash["updated"] = Time.now.to_i - TIME_TO_LIVE - 1
           test_hash.expired?.should be_true
         end
       end
 
-      it "should return true if self's updated time plus TIME_TO_LIVE is the same as the current time" do
+      it "should return false if self's updated time plus TIME_TO_LIVE is the same as the current time" do
         Timecop.freeze(Time.now) do
           test_hash["updated"] = Time.now.to_i - TIME_TO_LIVE
           test_hash.expired?.should be_false
         end
       end
 
-      it "should return true if self's updated time plus TIME_TO_LIVE is after the current time" do
+      it "should return false if self's updated time plus TIME_TO_LIVE is after the current time" do
         Timecop.freeze(Time.now) do
           test_hash["updated"] = Time.now.to_i - TIME_TO_LIVE + 1
           test_hash.expired?.should be_false
         end
       end
     end
+  end
+
+  it "should respond to GET" do
+    get '/'
+    last_response.should be_ok
+  end
+
+  it "should return the welcome page at root" do
+    get '/'
+    last_response.body.should include "Hi. I'm Omnivore, your friendly feed cache server."
   end
 end
