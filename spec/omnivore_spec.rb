6f455d7f5ed2c668618cfa8e2f088ff173816d8c
tshr <tshr@Toshiros-MacBook-Pro.local>
2013-06-25 21:22:46 -0700

    changed feed var name and added successful request example group

--- spec/omnivore_spec.rb
+++ spec/omnivore_spec.rb
@@ -55,41 +55,45 @@ describe "Omnivore" do
   describe "GET /feed" do
     context "requesting a valid feed URL" do
 
-      let(:valid_feed_url) {'http://www.example.com/feed.rss'}
+      shared_examples_for "a successful request" do |content_type|
+
+        before(:each) do
+          get '/feed?url=' + feed_url
+        end
+
+        it "responds OK" do
+          last_response.should be_ok
+        end
+
+        it "returns the response feed" do
+          last_response.body.should == "successful response"
+        end
+
+        it "returns a content type of xml" do
+          last_response.header["Content-Type"].should include "text/#{content_type}"
+        end
+      end
+
+      let(:feed_url) {'http://www.example.com/feed.rss'}
 
       context "and the feed isn't cached" do
         before(:each) do
-          Redis.any_instance.stub(:hgetall).with(valid_feed_url).and_return({})
-          RestClient.stub(:get).with(valid_feed_url).and_return("successful response")
+          Redis.any_instance.stub(:hgetall).with(feed_url).and_return({})
+          RestClient.stub(:get).with(feed_url).and_return("successful response")
+          Redis.any_instance.stub(:hmset)
         end
 
         it "stores the feed url and response in the database, with the 'count' set to
             1, and 'updated' set to the current time" do
-          Redis.any_instance.should_receive(:hmset).with(valid_feed_url, "feed",
+
+          Redis.any_instance.should_receive(:hmset).with(feed_url, "feed",
                                                          "successful response",
                                                          "count", 1, "updated",
                                                          Time.now.to_i)
-          get '/feed?url=' + valid_feed_url
+          get '/feed?url=' + feed_url
         end
 
-        context "returns the feed and an OKresponse" do
-          before(:each) do
-            Redis.any_instance.stub(:hmset)
-            get '/feed?url=' + valid_feed_url
-          end
-
-          it "responds OK" do
-            last_response.should be_ok
-          end
-
-          it "returns the response feed" do
-            last_response.body.should == "successful response"
-          end
-
-          it "returns a content type of xml" do
-            last_response.header["Content-Type"].should include "text/xml"
-          end
-        end
+        it_should_behave_like "a successful request", "xml"
       end
 
       context "and the feed is cached" do
@@ -99,46 +103,31 @@ describe "Omnivore" do
           let(:returned_feed_count) {"1"}
 
           before(:each) do
-            Redis.any_instance.stub(:hgetall).with(valid_feed_url).and_return(
+            Redis.any_instance.stub(:hgetall).with(feed_url).and_return(
               { "feed" => "cached feed",
                 "count" => returned_feed_count,
                 "updated" => older_than_time_to_live_ago.to_s
               }
             )
-            RestClient.stub(:get).with(valid_feed_url).and_return("successful response")
-            Redis.any_instance.stub(:hmset).with(valid_feed_url, "feed",
-                                                    "successful response",
-                                                    "count", returned_feed_count.to_i + 1, "updated",
-                                                    Time.now.to_i)
+
+            RestClient.stub(:get).with(feed_url).and_return("successful response")
+            Redis.any_instance.stub(:hmset)
           end
 
           it "resets the feed hash with the refreshed response, increments the
               count by one and sets 'updated' to the current time" do
-            Redis.any_instance.should_receive(:hmset).with(valid_feed_url, "feed",
+            Redis.any_instance.should_receive(:hmset).with(feed_url, "feed",
                                                            "successful response",
                                                            "count", returned_feed_count.to_i + 1, "updated",
                                                            Time.now.to_i)
-            get '/feed?url=' + valid_feed_url
+            get '/feed?url=' + feed_url
           end
 
-          it "responds OK" do
-            get '/feed?url=' + valid_feed_url
-            last_response.should be_ok
-          end
-
-          it "returns the response feed" do
-            get '/feed?url=' + valid_feed_url
-            last_response.body.should == "successful response"
-          end
-
-          it "returns a content type of xml" do
-            get '/feed?url=' + valid_feed_url
-            last_response.header["Content-Type"].should include "text/xml"
-          end
+          it_should_behave_like "a successful request", "xml"
         end
         context "and it isn't expired" do
           before(:each) do
-            Redis.any_instance.stub(:hgetall).with(valid_feed_url).and_return(
+            Redis.any_instance.stub(:hgetall).with(feed_url).and_return(
               { "feed" => "cached feed",
                 "count" => "1",
                 "updated" => younger_than_time_to_live_ago.to_s
@@ -147,25 +136,25 @@ describe "Omnivore" do
           end
 
           it "increments the feed's count by 1" do
-            Redis.any_instance.should_receive(:hincrby).with(valid_feed_url, "count", 1)
-            get '/feed?url=' + valid_feed_url
+            Redis.any_instance.should_receive(:hincrby).with(feed_url, "count", 1)
+            get '/feed?url=' + feed_url
           end
 
           it "responds OK" do
-            Redis.any_instance.stub(:hincrby).with(valid_feed_url, "count", 1)
-            get '/feed?url=' + valid_feed_url
+            Redis.any_instance.stub(:hincrby).with(feed_url, "count", 1)
+            get '/feed?url=' + feed_url
             last_response.should be_ok
           end
 
           it "returns the cached feed" do
-            Redis.any_instance.stub(:hincrby).with(valid_feed_url, "count", 1)
-            get '/feed?url=' + valid_feed_url
+            Redis.any_instance.stub(:hincrby).with(feed_url, "count", 1)
+            get '/feed?url=' + feed_url
             last_response.body.should == "cached feed"
           end
 
           it "returns a content type of xml" do
-            Redis.any_instance.stub(:hincrby).with(valid_feed_url, "count", 1)
-            get '/feed?url=' + valid_feed_url
+            Redis.any_instance.stub(:hincrby).with(feed_url, "count", 1)
+            get '/feed?url=' + feed_url
             last_response.header["Content-Type"].should include "text/xml"
           end
         end
