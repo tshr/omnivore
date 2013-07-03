66d26e86e776929c503b676a7614e5efc988a128
tshr <tshr@Toshiros-MacBook-Pro.local>
2013-07-02 20:01:49 -0700

    consolidated example group to one expectation

--- spec/shared_examples.rb
+++ spec/shared_examples.rb
@@ -1,18 +1,10 @@
 shared_examples_for "a successful request" do |request, content, content_type, include = false|
 
-  before(:each) do
+  it "returns an OK response with the correct content type and the expected response body" do
     get request
-  end
-
-  it "responds OK" do
     last_response.should be_ok
-  end
-
-  it "returns the expected response feed" do
+    last_response.header["Content-Type"].should include content_type
     include ? (last_response.body.should include content) : (last_response.body.should eq content)
   end
 
-  it "returns the expected content type" do
-    last_response.header["Content-Type"].should include content_type
-  end
 end
