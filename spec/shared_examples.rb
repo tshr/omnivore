41c28a9433cc94bca303d7ab8bcff1e614955f3d
tshr <toshirokensugihara@Toshiros-MacBook-Pro.local>
2013-10-12 17:15:06 -0700

    changed include var to include_test

--- spec/shared_examples.rb
+++ spec/shared_examples.rb
@@ -1,10 +1,10 @@
-shared_examples_for "a successful request" do |request, content, content_type, include = false|
+shared_examples_for "a successful request" do |request, content, content_type, include_test = false|
 
   it "returns an OK response with the correct content type and the expected response body" do
     get request
     last_response.should be_ok
     last_response.header["Content-Type"].should include content_type
-    include ? (last_response.body.should include content) : (last_response.body.should eq content)
+    include_test ? (last_response.body.should include content) : (last_response.body.should eq content)
   end
 
 end
