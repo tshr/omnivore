f7851c40618ba3b4860ecbf395a498c94dd66bb4
tshr <toshirokensugihara@Toshiros-MacBook-Pro.local>
2013-11-17 20:25:25 -0500

    put return back in

--- omnivore.rb
+++ omnivore.rb
@@ -73,7 +73,7 @@ helpers do
       response = RestClient.get request_url
     rescue
       content_type :json
-      { error: "Could not connect to source." }.to_json
+      return { error: "Could not connect to source." }.to_json
     end
 
     # Updated time value stored in Unix epoch seconds
